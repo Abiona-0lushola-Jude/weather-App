822a72118461b0d91b9483a7393d4990f9d0f4ef
Abiona Olushola Jude <abionaolushola11@yahoo.com>
2022-06-13 16:53:47 +0100

    reviewing

--- main.js
+++ main.js
@@ -1,44 +1,51 @@
-let colorArray = [
-    {
-        color: 'blue'
-    },
-    {
-        color: 'red'
-    },
-    {
-        color:'darkgreen'
-    },
-    {
-        color:'gray'
-    },
-    {
-        color:'maroon'
-    }
-]
-const btn = document.querySelector('#btn')
-if (btn.addEventListener == true){
-    getWeather()
-}
+// My Buttons
+const menuBtn = document.querySelector('.menu-btn')
+const menu  = document.querySelector('nav')
+// My inputs
+const humidity = document.querySelector('#humidity-result')
+const pressure = document.querySelector('#pressure-result')
+const headerLocation = document.querySelector('#header-location')
+const data = document.querySelector('#date-taken')
+const search = document.querySelector('#search')
+const tempValue = document.querySelector('#temp-reading')
+const tempMainValue = document.querySelector('#temp-texting')
+const lastLocation = document.querySelector('#last-location')
+const lastDate = document.querySelector('#last-date')
+const cloudyText = document.querySelector('#cloud-text')
+const indexText = document.querySelector('#Index-result')
+const windText = document.querySelector('#wind-result')
+const probabilityText = document.querySelector('#probability-result')
+const image = document.querySelector('#new-Image')
+const realFeel = document.querySelector('#real')
+const input = document.querySelector('input')
 
-btn.addEventListener('click', getWeather)
+function toggleMenu(){
+    menuBtn.classList.toggle('open')
+    menu.classList.toggle('open')
+}
+// clicking of the buttons
+menuBtn.addEventListener('click', toggleMenu)
+
+
+
+
 function getWeather(){
-     
-    const input = document.querySelector('#input').value
-        fetch(`https://weatherdbi.herokuapp.com/data/weather/${input}`)
+         let value = input.value
+        fetch(`https://weatherdbi.herokuapp.com/data/weather/london`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            document.querySelector('img').src = data.currentConditions.iconURL
-            document.querySelector('#humidity').textContent = data.currentConditions.humidity
-            document.querySelector('#country').textContent = data.region
-            document.querySelector('#wind').textContent = data.currentConditions.wind.km +'km'
-            document.querySelector('#date').textContent = data.currentConditions.dayhour
-            document.querySelector('#weatherText').textContent = data.currentConditions.comment
-            document.querySelector('#tempOne').textContent = data.currentConditions.temp.c+'c'
-            document.querySelector('#tempTwo').textContent = data.currentConditions.temp.f+'f'
-            document.querySelector('#preci').textContent = data.currentConditions.precip
+            console.log(data.currentConditions.comment)
+            cloudyText.textContent = data.currentConditions.comment
+            tempMainValue.textContent = data.currentConditions.temp.c
+            tempValue.textContent = data.currentConditions.temp.c
+            humidity.textContent = data.currentConditions.humidity
+            windText.textContent = `${data.currentConditions.wind.km} km`
+            image.src = data.currentConditions.iconURL
         }) 
         .catch(err => console.error(err));
 	
 }
 
+
+search.addEventListener('click',getWeather)
---
+++ sub-main.js
@@ -0,0 +1,7 @@
+// function loadScript(src){
+//     const script = document.createElement("script")
+//     script.src = src
+//     document.head.prepend(script)
+// }
+
+// loadScript('main.js')
