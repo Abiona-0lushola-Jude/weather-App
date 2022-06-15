a54804c80a95eb9b352eec690528a6b85350e55c
Abiona Olushola Jude <abionaolushola11@yahoo.com>
2022-06-15 09:57:06 +0100

    done

--- subb-main.js
+++ subb-main.js
@@ -1,15 +1,15 @@
 // My Buttons
-const menuBtn = document.querySelector('.menu-btn')
-const menu  = document.querySelector('nav')
-// My inputs
+// const menuBtn = document.querySelector('.menu-btn')
+// const menu  = document.querySelector('nav')
+// // My inputs
 
-const search = document.querySelector('#search')
-function toggleMenu(){
-    menuBtn.classList.toggle('open')
-    menu.classList.toggle('open')
-}
-// clicking of the buttons
-menuBtn.addEventListener('click', toggleMenu)
+// const search = document.querySelector('#search')
+// function toggleMenu(){
+//     menuBtn.classList.toggle('open')
+//     menu.classList.toggle('open')
+// }
+// // clicking of the buttons
+// menuBtn.addEventListener('click', toggleMenu)
 
 const lastLocation = document.querySelector('#last-location')
 const textReading = document.querySelector('#temp-reading')
